2f9f0c0bedf6761a6b1ebbbb8c6e99ee81020824
BuildTools <tawatson@live.com.au>
2017-10-22 13:56:09 +1100

    Added minifying JS and jQuery compiler into gulp. Added first bits of code for site design. Added fancy video background with deferred jQuery loading.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect'),
-  watch = require('gulp-watch'),
-  less = require('gulp-less');
+    gutil = require('gulp-util'),
+    less = require('gulp-less'),
+    connect = require('gulp-connect'),
+    watch = require('gulp-watch'),
+    plugins = require('gulp-load-plugins')({
+      rename: {
+        'gulp-jshint': 'jshint'
+      }
+    });
 
 gulp.task('webserver', function() {
   connect.server({
@@ -16,6 +22,32 @@ gulp.task('livereload', function() {
     .pipe(connect.reload());
 });
 
+// Minify jQuery Plugins: Run manually with: "gulp squish-jquery"
+gulp.task('squish-jquery', function () {
+    return gulp.src('js/prefab/libs/**/*.js')
+        .pipe(plugins.uglify({
+            output: {
+                'ascii_only': true
+            }
+        }))
+        .pipe(plugins.concat('jquery.plugins.min.js'))
+        .pipe(gulp.dest('js'));
+});
+
+// Minify Custom JS: Run manually with: "gulp build-js"
+gulp.task('build-js', function () {
+    return gulp.src('js/prefab/*.js')
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.uglify({
+            output: {
+                'ascii_only': true
+            }
+        }))
+        .pipe(plugins.concat('scripts.min.js'))
+        .pipe(gulp.dest('js'));
+});
+
 gulp.task('less', function() {
   gulp.src('css/prefab/style.less')
     .pipe(less())
@@ -24,6 +56,8 @@ gulp.task('less', function() {
 
 gulp.task('watch', function() {
   gulp.watch('css/prefab/*.less', ['less']);
+  gulp.watch('js/prefab/*.js', ['build-js']);
+  gulp.watch('js/prefab/libs/**/*.js',['squish-jquery']);
 })
 
-gulp.task('default', ['less', 'webserver', 'livereload', 'watch']);
+gulp.task('default', ['less', 'build-js', 'squish-jquery', 'webserver', 'livereload', 'watch']);
